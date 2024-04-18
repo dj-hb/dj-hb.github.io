07295a2e313b21d3e7e21817e250ae6e7300c2b8
dj-hb <urban_seonbi@naver.com>
2024-04-18 17:57:26 +0900

    pwa test 3

--- sw.js
+++ sw.js
@@ -3,4 +3,39 @@ const cacheAsset = ["index.html", "main.js", "style.css"]
 
 self.addEventListener("install", (e) => {
   console.log("service worker: installed")
+
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("service worker: caching files")
+      cache.addAll(cacheAsset)
+    }).then(() => self.skipWaiting())
+  )
+
+})
+
+self.addEventListener("activate", (e) => {
+  console.log("service worker: activated")
+
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            console.log("service worker: clearing old cache")
+            return caches.delete(cache)
+          }
+        })
+      )
+    })
+  )
+
+})
+
+self.addEventListener("fetch", (e) => {
+  console.log("service worker: fetching")
+
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  )
+  
 })
