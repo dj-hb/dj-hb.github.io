52f5971d3378ba1e8d0a7af8d5e64b9d2e34b473
dj-hb <urban_seonbi@naver.com>
2024-04-18 17:14:31 +0900

    pwa test 2

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ setTimeout(() => {
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./sw.js")
+    navigator.serviceWorker.register("./sw.js").then((registration) => {
+      console.log("service worker: registered")
+    }).catch((err) => {
+      console.error(`(service worker register error) ${err}`)
+    })
   })
 }
