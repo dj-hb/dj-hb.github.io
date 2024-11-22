44ad30a283a099722fe9b27984fbd431b025c81a
dj-hb <urban_seonbi@naver.com>
2024-11-22 13:15:50 +0900

    firestore 연결 확인

--- vue2_project/fcm-test/src/components/HelloWoldo.vue
+++ vue2_project/fcm-test/src/components/HelloWoldo.vue
@@ -1,15 +1,53 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="item in tstArr">{{ item }}</li>
+    </ul>
   </div>
 </template>
 
 
 <script>
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+
 export default {
-  name: 'HelloWorld',
+  name: 'HelloWoldo',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      tstArr: [],
+    }
+  },
+  async created() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAxwCiFs8F4cbP1mZvEs1iaeKRimSPvyZo",
+      authDomain: "fcm-test-9e465.firebaseapp.com",
+      projectId: "fcm-test-9e465",
+      storageBucket: "fcm-test-9e465.firebasestorage.app",
+      messagingSenderId: "38271062035",
+      appId: "1:38271062035:web:d682213c40e7d5688b6fe6",
+      measurementId: "G-TGN6LLRD3D"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    
+    const db = getFirestore(app);
+    
+    const querySnapshot = await getDocs(collection(db, "202411221257"));
+    querySnapshot.forEach((doc) => {
+      // console.log(doc)
+      // console.log(`${doc.id} => ${doc.data()}`);
+      console.log(`${doc.id} => ${doc.data().drinkName}`);
+      this.tstArr.push(doc.data().drinkName);
+    });
+    
+
   }
 }
 </script>
