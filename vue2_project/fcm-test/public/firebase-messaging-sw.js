8382f9507ee500beea759ff108d22d630daf633c
dj-hb <urban_seonbi@naver.com>
2024-11-22 14:13:41 +0900

    fcm 서비스워커...

--- vue2_project/fcm-test/public/firebase-messaging-sw.js
+++ vue2_project/fcm-test/public/firebase-messaging-sw.js
@@ -1,3 +1,18 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAxwCiFs8F4cbP1mZvEs1iaeKRimSPvyZo",
+  authDomain: "fcm-test-9e465.firebaseapp.com",
+  projectId: "fcm-test-9e465",
+  storageBucket: "fcm-test-9e465.firebasestorage.app",
+  messagingSenderId: "38271062035",
+  appId: "1:38271062035:web:d682213c40e7d5688b6fe6",
+  measurementId: "G-TGN6LLRD3D"
+})
+
+const messaging = firebase.messaging();
+
 self.addEventListener("install", function (e) {
   self.skipWaiting();
 });
