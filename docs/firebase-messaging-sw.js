f6465174ac2f5905d88a565c5e928712180f537a
dj-hb <urban_seonbi@naver.com>
2025-01-08 21:24:04 +0900

    build: 2차 테스트 배포

--- docs/firebase-messaging-sw.js
+++ docs/firebase-messaging-sw.js
@@ -1,24 +1,59 @@
-self.addEventListener("install", function (e) {
-  self.skipWaiting();
-});
+importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging-compat.js');
 
-self.addEventListener("activate", function (e) {
-  console.log("fcm sw activated..");
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyAxwCiFs8F4cbP1mZvEs1iaeKRimSPvyZo",
+  authDomain: "fcm-test-9e465.firebaseapp.com",
+  projectId: "fcm-test-9e465",
+  storageBucket: "fcm-test-9e465.firebasestorage.app",
+  messagingSenderId: "38271062035",
+  appId: "1:38271062035:web:d682213c40e7d5688b6fe6",
+  measurementId: "G-TGN6LLRD3D"
+};
 
-self.addEventListener("push", function (e) {
-  if (!e.data.json()) return;
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
 
-  const resultData = e.data.json().notification;
-  const notificationTitle = resultData.title;
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('https://dj-hb.github.io')
+  );
+})
 
+messaging.onBackgroundMessage(function(payload) {
+  // console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
   const notificationOptions = {
-    body: resultData.body,
+    body: 'Background Message body.',
+    // icon: '/firebase-logo.png'
   };
 
-  console.log(resultData.title, {
-    body: resultData.body,
-  });
-
-  e.waitUntil(self.registration.showNotification(notificationTitle, notificationOptions));
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
+
+// self.addEventListener("install", function (e) {
+//   self.skipWaiting();
+// });
+
+// self.addEventListener("activate", function (e) {
+//   console.log("fcm sw activated..");
+// });
+
+// self.addEventListener("push", function (e) {
+//   if (!e.data.json()) return;
+
+//   const resultData = e.data.json().notification;
+//   const notificationTitle = resultData.title;
+
+//   const notificationOptions = {
+//     body: resultData.body,
+//   };
+
+//   console.log(resultData.title, {
+//     body: resultData.body,
+//   });
+
+//   e.waitUntil(self.registration.showNotification(notificationTitle, notificationOptions));
+// });
