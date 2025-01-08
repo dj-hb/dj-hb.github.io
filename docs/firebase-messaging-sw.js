40440860bfeb5a934140b0f5f53789a9559d78ef
dj-hb <urban_seonbi@naver.com>
2025-01-08 21:37:11 +0900

    build: 3차 테스트 배포

--- docs/firebase-messaging-sw.js
+++ docs/firebase-messaging-sw.js
@@ -1,18 +1,18 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-app-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging-compat.js');
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAxwCiFs8F4cbP1mZvEs1iaeKRimSPvyZo",
-  authDomain: "fcm-test-9e465.firebaseapp.com",
-  projectId: "fcm-test-9e465",
-  storageBucket: "fcm-test-9e465.firebasestorage.app",
-  messagingSenderId: "38271062035",
-  appId: "1:38271062035:web:d682213c40e7d5688b6fe6",
-  measurementId: "G-TGN6LLRD3D"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAxwCiFs8F4cbP1mZvEs1iaeKRimSPvyZo",
+//   authDomain: "fcm-test-9e465.firebaseapp.com",
+//   projectId: "fcm-test-9e465",
+//   storageBucket: "fcm-test-9e465.firebasestorage.app",
+//   messagingSenderId: "38271062035",
+//   appId: "1:38271062035:web:d682213c40e7d5688b6fe6",
+//   measurementId: "G-TGN6LLRD3D"
+// };
 
-const app = firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
+// const app = firebase.initializeApp(firebaseConfig);
+// const messaging = firebase.messaging();
 
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
@@ -21,39 +21,39 @@ self.addEventListener('notificationclick', function(event) {
   );
 })
 
-messaging.onBackgroundMessage(function(payload) {
-  // console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    // icon: '/firebase-logo.png'
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
-// self.addEventListener("install", function (e) {
-//   self.skipWaiting();
-// });
-
-// self.addEventListener("activate", function (e) {
-//   console.log("fcm sw activated..");
-// });
-
-// self.addEventListener("push", function (e) {
-//   if (!e.data.json()) return;
-
-//   const resultData = e.data.json().notification;
-//   const notificationTitle = resultData.title;
-
+// messaging.onBackgroundMessage(function(payload) {
+//   // console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//   // Customize notification here
+//   const notificationTitle = 'Background Message Title';
 //   const notificationOptions = {
-//     body: resultData.body,
+//     body: 'Background Message body.',
+//     // icon: '/firebase-logo.png'
 //   };
 
-//   console.log(resultData.title, {
-//     body: resultData.body,
-//   });
-
-//   e.waitUntil(self.registration.showNotification(notificationTitle, notificationOptions));
+//   self.registration.showNotification(notificationTitle, notificationOptions);
 // });
+
+self.addEventListener("install", function (e) {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (e) {
+  console.log("fcm sw activated..");
+});
+
+self.addEventListener("push", function (e) {
+  if (!e.data.json()) return;
+
+  const resultData = e.data.json().notification;
+  const notificationTitle = resultData.title;
+
+  const notificationOptions = {
+    body: resultData.body,
+  };
+
+  console.log(resultData.title, {
+    body: resultData.body,
+  });
+
+  e.waitUntil(self.registration.showNotification(notificationTitle, notificationOptions));
+});
