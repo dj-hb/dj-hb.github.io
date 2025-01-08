afd89e4d8e4ba3d8d75fac181ad679a1555c7b73
dj-hb <urban_seonbi@naver.com>
2025-01-08 20:25:05 +0900

    feat: 빌드 경로 수정

--- hello-woldo/vue.config.js
+++ hello-woldo/vue.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 8090,
-  }
+  },
+  outputDir: path.resolve(__dirname, '../../docs'),
 })
