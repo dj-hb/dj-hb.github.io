02d8f866163860895059651e17ba84baeecc5c97
dj-hb <urban_seonbi@naver.com>
2025-01-08 18:06:49 +0900

    feat: 테스트용 컴포넌트 생성

---
+++ hello-woldo/src/components/HelloWoldo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="hello-woldo">
+    <div>{{ msg }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWoldo',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      
+    }
+  },
+  created() {
+
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style scoped>
+#hello-woldo {
+  padding: 10px;
+  text-align: center;
+}
+</style>
